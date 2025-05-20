c949e4350e71065cc2d78e8981b78d2d4e8d6351
Sarah06102 <sarah.hra06@gmail.com>
2025-05-20 12:08:21 -0700

    Initial portfolio upload

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,8 +9,8 @@ export const About = () => {
                     </span>
                 </h2>
                 <p>
-                I'm Sarah Hussain, a Management Engineering student at the University of Waterloo with a passion for building thoughtful, tech-powered solutions. Whether it's a full-stack app, a data dashboard, or an automation script, I love turning ideas into tools that solve real problems. 
-                My background blends engineering, design, and business - giving me a systems-level mindset and a bias toward execution. I’ve worked on everything from front-end experiences in React to backend workflows in Python and SQL.
+                I'm Sarah Hussain, a Management Engineering student at the University of Waterloo with a passion for building thoughtful and tech-powered solutions. Whether it's a full-stack app or a data dashboard I love turning ideas into tools that solve real problems. 
+                My background blends engineering, design, and business, which gives me a systems-level mindset and a bias toward execution. I’ve worked on everything from front-end experiences in React to backend workflows in Python and SQL.
                 I’m currently looking for opportunities to collaborate on high-impact projects where I can grow as a developer and contribute to teams that care about both people and product.
                 </p>
 
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -7,18 +7,15 @@ const ProjectData = [
     {
         name: `TrackMyApps`,
         description: `A Chrome extension that allows users to save job postings from platforms like LinkedIn directly to a personalized dashboard. With one click, users can capture job details and sync them to a real-time web app powered by Firebase. Built to simplify job tracking, reduce tab clutter, and help users stay organized throughout their job search.`,
-        skills: ["React", "Firebase", "Chrome API", "HTML", "CSS"],
-        // github:
+        skills: ["React", "Firebase", "Chrome API", "HTML", "Tailwind CSS"],
+        github: `https://github.com/Sarah06102/TrackMyApps`,
     },
     {
         name: `AutoTrack`,
+        status: `(in progress)`,
         description: `A web-based expense tracker that helps users automate their spending logs, set budgets, and visualize financial habits. AutoTrack syncs in real time and offers personalized alerts, budget insights, and a clean, user-friendly interface to simplify money management and encourage smarter financial decisions.`,
         skills: ["HTML", "CSS", "Django", "React"],
         // github:
-    },
-    {
-        name: `Name`,
-        description: `add content`
     },
 ]
 
@@ -27,7 +24,7 @@ export const Projects = () => {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
+        slidesToShow: 2,
         slidesToScroll: 1,
         responsive: [
             {
@@ -49,15 +46,17 @@ export const Projects = () => {
                 </h2>
             </div>
 
-            <div className="w-3/4 m-auto">
+            <div className="w-full px-4 md:px-8">
                 <div className={`relative w-full max-w-6xl mx-auto mt-10 ${isDarkTheme ? 'dark-slider' : 'light-slider'}`}>
                     <Slider {...settings}>
                     {ProjectData.map((p, i) => (
                         <div key={i}>
-                            <div className={`min-w-[280px] max-w-[90vw] h-[450px] rounded-xl p-6 m-2 card-transform hover:shadow-lg ${isDarkTheme ? 'dark-card' : 'light-card'}`}>
-                                <div className="flex flex-col justify-between items-center h-full gap-4 p-4">
-                                    <p className="text-xl font-semibold mb-2 text-center">{p.name}</p>
-                                    <p className="text-sm text-center break-words max-w-[260px]">{p.description}</p>
+                            <div className={`min-w-[280px] max-w-[90vw] h-[500px] rounded-xl p-10 m-2 card-transform hover:shadow-lg ${isDarkTheme ? 'dark-card' : 'light-card'}`}>
+                                <div className="flex flex-col justify-between items-center h-full gap-4 p-3">
+                                    <p className="text-xl font-bold mt-2 underline text-center">{p.name}</p>
+                                    <p className="text-sm text-center break-words max-w-[260px]">{p.status}</p>
+                                    <p className="text-sm text-center break-words max-w-[350px]">{p.description}</p>
+                                    
                                    
                                     {/* Skills */}
                                 <div className="flex flex-wrap justify-center gap-2 md:mb-6">
@@ -69,7 +68,7 @@ export const Projects = () => {
 
                                     {/* Github Link */}
                                 </div>
-                                    <a className="format hover:text-indigo-400 mt-0 -mb-2 text-center hover:underline text-primary md:mt-2 md:-mb-4" href={p.github || '#'} target="_blank" rel="noopener noreferrer">GitHub</a>
+                                    <a className="format hover:text-indigo-400 text-center hover:underline text-primary md:mt-2 md:-mb-4" href={p.github || '#'} target="_blank" rel="noopener noreferrer">GitHub</a>
                                 </div>
                             </div>
                         </div>
